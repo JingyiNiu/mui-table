cd455d75a603512b220146a2e00d6dc35b5ff03e
JingyiNiu <niu.jy1203@gmail.com>
2022-05-02 12:11:09 +1200

    update nav

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import "./App.css";
 import HomePage from "./pages/home/home";
 import EmployerPage from "./pages/employers/employers";
 import JobsPage from "./pages/jobs/jobs";
+import Nav from "./components/nav/nav";
 
 function App() {
   return (
     <div>
       <Router>
+      <Nav />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/jobs" element={<JobsPage />} />
---
+++ src/components/mui-table/mui-table.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { lighten, makeStyles } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+const MuiTable = () => {
+  return (
+    <div>MuiTable</div>
+  )
+}
+
+export default MuiTable
